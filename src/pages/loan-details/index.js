d798f8853785cc58d6d4b4b093c95cd2d04c99ba
Nate Schultz <nate.schultz@outlook.com>
2023-07-24 10:17:12 -0400

    worked on table spacing

--- src/pages/loan-details/index.js
+++ src/pages/loan-details/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import { Typography, Grid, Paper } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Typography, Grid, Paper, Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import { XYPlot, XAxis, YAxis, VerticalBarSeries, LineSeries } from 'react-vis';
-
 import { useParams } from 'react-router-dom';
 import { useAuth } from 'pages/authentication/auth-forms/AuthProvider';
-import { useState, useEffect } from 'react';
 import ImageComponent from 'components/ImageUUID';
 import LoanRepaymentChart from 'pages/loan-details/LoanRepaymentChart';
 
@@ -35,20 +33,10 @@ const LoanDetails = () => {
         return <div>Loading...</div>;
     }
 
-    console.log(loanData);
-
     const paymentData = loanData.repaymentSchedule.map((payment, index) => {
-        // Assuming the amountDue for each payment includes both principal and interest,
-        // we compute the cumulative total paid up to the current index
         const cumulativePaid = loanData.repaymentSchedule.slice(0, index + 1).reduce((sum, p) => sum + p.amountDue, 0);
-
-        // The cumulative principal paid is the initial loan amount divided by the number of total payments, times the number of payments made
         const cumulativePrincipalPaid = (loanData.principalAmount / loanData.repaymentSchedule.length) * (index + 1);
-
-        // The cumulative interest paid is the difference between the cumulative total paid and the cumulative principal paid
         const cumulativeInterestPaid = cumulativePaid - cumulativePrincipalPaid;
-
-        // The remaining debt is the total of all payments minus the cumulative total paid
         const remainingDebt = loanData.repaymentSchedule.reduce((sum, p) => sum + p.amountDue, 0) - cumulativePaid;
 
         return {
@@ -59,56 +47,69 @@ const LoanDetails = () => {
         };
     });
 
-    console.log(paymentData);
-
     return (
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <Paper>
-                    <Grid container>
-                        <Grid item xs={3}>
-                            <ImageComponent ipfsLink={loanData.metadata.loanImageLink} size="large" />
-                        </Grid>
-                        <Grid item xs={9}>
-                            <Typography variant="h5">Principal Amount: {loanData.principalAmount}</Typography>
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Grid>
+        <Box pt={2} px={2}>
+            <Typography variant="h4">Loan Details</Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper>
+                        <Box p={2}>
+                            <Grid container>
+                                <Grid item xs={3}>
+                                    <Box m={1}>
+                                        <ImageComponent ipfsLink={loanData.metadata.loanImageLink} size="medium" />
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={9}>
+                                    <Typography variant="h5">Principal Amount: {loanData.principalAmount}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Paper>
+                </Grid>
 
-            <Grid item xs={12}>
-                <Paper>
-                    <Typography variant="h6">Repayment Schedule</Typography>
-                    <LoanRepaymentChart data={paymentData} />
-                </Paper>
-            </Grid>
+                <Grid item xs={12}>
+                    <Paper>
+                        <Box p={2}>
+                            <Typography variant="h6">Repayment Schedule</Typography>
+                            <LoanRepaymentChart data={paymentData} />
+                        </Box>
+                    </Paper>
+                </Grid>
 
-            <Grid item xs={12}>
-                <Paper>
-                    <Typography variant="h6">Payment Details</Typography>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID Image</th>
-                                <th>Amount Due</th>
-                                <th>Due Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {loanData.repaymentSchedule.map((payment) => (
-                                <tr key={payment.paymentId}>
-                                    <td>
-                                        <ImageComponent ipfsLink={payment.imageLink} />
-                                    </td>
-                                    <td>{payment.amountDue}</td>
-                                    <td>{new Date(payment.dueDate).toLocaleDateString()}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </Paper>
+                <Grid item xs={12}>
+                    <Paper>
+                        <Box p={2}>
+                            <Typography variant="h6">Payment Details</Typography>
+                            <TableContainer>
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>ID Image</TableCell>
+                                            <TableCell>Amount Due</TableCell>
+                                            <TableCell>Due Date</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {loanData.repaymentSchedule.map((payment) => (
+                                            <TableRow key={payment.paymentId}>
+                                                <TableCell>
+                                                    <Box m={1}>
+                                                        <ImageComponent ipfsLink={payment.imageLink} size="medium" />
+                                                    </Box>
+                                                </TableCell>
+                                                <TableCell>{payment.amountDue}</TableCell>
+                                                <TableCell>{new Date(payment.dueDate).toLocaleDateString()}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Box>
+                    </Paper>
+                </Grid>
             </Grid>
-        </Grid>
+        </Box>
     );
 };
 
