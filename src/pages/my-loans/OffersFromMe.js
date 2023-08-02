1b3a9e44500e08becabddbcd3d3cdf8a2a0ed29c
Nate Schultz <nate.schultz@outlook.com>
2023-08-01 21:33:58 -0400

    misc fixes

--- src/pages/my-loans/OffersFromMe.js
+++ src/pages/my-loans/OffersFromMe.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Button, Modal, Tooltip } from 'antd';
+import { Table, Button, Modal, Tooltip, Input } from 'antd';
 import moment from 'moment';
 import { useAuth } from 'pages/authentication/auth-forms/AuthProvider';
 import { load_endpoint } from 'pages/my-loans';
@@ -12,6 +12,7 @@ const OffersFromMe = () => {
     const navigate = useNavigate();
     const [modalVisible, setModalVisible] = useState(false);
     const [depositAddress, setDepositAddress] = useState('');
+    const [selectedPrincipal, setSelectedPrincipal] = useState(0);
 
     useEffect(() => {
         setDataLoading(true);
@@ -77,7 +78,7 @@ const OffersFromMe = () => {
         },
         {
             title: 'Loan Status',
-            dataIndex: 'loan_status'
+            render: (text) => 'DRAFT'
         },
         {
             title: 'Action',
@@ -88,6 +89,7 @@ const OffersFromMe = () => {
                     onClick={() => {
                         setDepositAddress(record.borrower_deposit_wallet);
                         setModalVisible(true);
+                        setSelectedPrincipal(record.principal);
                     }}
                 >
                     Submit Offer
@@ -104,14 +106,47 @@ const OffersFromMe = () => {
         setModalVisible(false);
     };
 
+    const checkPaymentCompleted = () => {
+        const paymentCheckURL = 'http://127.0.0.1:8000/payments/completed?address=' + depositAddress;
+
+        load_endpoint(
+            user,
+            paymentCheckURL,
+            (result) => {
+                if (result) {
+                    alert('Payment has been completed');
+                } else {
+                    alert('Payment not yet completed');
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    };
+
+    const copyAddressToClipboard = () => {
+        navigator.clipboard.writeText(depositAddress);
+    };
+
     return (
         <div>
             <Table columns={columns} dataSource={items} loading={dataLoading} rowKey="loan" />
             <Modal title="Submit Offer" visible={modalVisible} onOk={handleModalOk} onCancel={handleModalCancel}>
                 <p>To submit this offer, please deposit the required amount to the following address:</p>
+                <Input
+                    value={depositAddress}
+                    readOnly
+                    addonAfter={
+                        <Button type="link" onClick={copyAddressToClipboard}>
+                            Copy
+                        </Button>
+                    }
+                />
                 <p>
-                    <strong>{depositAddress}</strong>
+                    Amount to deposit: <strong>{selectedPrincipal}</strong>
                 </p>
+                <Button onClick={checkPaymentCompleted}>Check if payment is completed</Button>
             </Modal>
         </div>
     );
