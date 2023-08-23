e2752cf328252863469400b7afd5774ced738056
Nate Schultz <nate.schultz@outlook.com>
2023-08-22 21:14:57 -0400

    added pages for basic endpoints

--- src/pages/applications/index.js
+++ src/pages/applications/index.js
@@ -1,5 +1,38 @@
-const Application = () => {
-    // TODO
+import { useState, useEffect, useRef, useContext } from 'react';
+
+// ant design
+import { Table } from 'antd';
+
+// project import
+import { useAuth } from 'pages/authentication/auth-forms/AuthProvider';
+import load_endpoint from 'utils/load_endpoint';
+
+const columns = [];
+
+const Applications = () => {
+    const [dataLoading, setLoading] = useState(false);
+    const [items, setItems] = useState([]);
+    const { user, loading } = useAuth();
+
+    useEffect(() => {
+        load_endpoint(
+            user,
+            'http://127.0.0.1:8000/loan/application?recent=True',
+            (result) => {
+                setItems(result);
+                setLoading(false);
+            },
+            (error) => {
+                setLoading(false);
+            }
+        );
+    }, []);
+
+    return (
+        <div>
+            <Table columns={columns} dataSource={[]} />
+        </div>
+    );
 };
 
-export default Application;
+export default Applications;
--- src/pages/loans/index.js
+++ src/pages/loans/index.js
@@ -5,26 +5,7 @@ import { Table } from 'antd';
 
 // project import
 import { useAuth } from 'pages/authentication/auth-forms/AuthProvider';
-
-const load_endpoint = (user, url, success_callback, failure_callback) => {
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${user.token}`,
-            'X-User-Uid': `${user.uid}`
-        }
-    })
-        .then((res) => res.json())
-        .then(
-            (result) => {
-                success_callback(result);
-            },
-            (error) => {
-                failure_callback(error);
-            }
-        );
-};
+import load_endpoint from 'utils/load_endpoint';
 
 const columns = [];
 
